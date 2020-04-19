52a0d925f1cd7e07b485f2dd0109cb1aa56a605d
Panelata <32406174+Panelata@users.noreply.github.com>
2020-04-20 01:32:30 +1000

    Fixed templates directory

--- RestaurantBooking/settings.py
+++ RestaurantBooking/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant.apps.RestaurantConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'RestaurantBooking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- restaurant/views.py
+++ restaurant/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index page.")
+    #return HttpResponse("Hello, world. You're at the index page.")
+    return render(request, 'index.html')
 # Create your views here.
