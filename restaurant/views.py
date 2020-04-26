d4aae95eec81efaee4f339fe28c6978c7f5963f2
Panelata <32406174+Panelata@users.noreply.github.com>
2020-04-26 19:30:43 +1000

    Added view_menu template and added basic grid

--- restaurant/views.py
+++ restaurant/views.py
@@ -5,3 +5,6 @@ def index(request):
     #return HttpResponse("Hello, world. You're at the index page.")
     return render(request, 'index.html')
 # Create your views here.
+
+def view_menu(request):
+    return render(request, 'view_menu.html')
