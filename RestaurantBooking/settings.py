ca5261fb02df996d6712965bdf73dc08ffa8cf57
David De Angelis <daviddeangelis@Davids-MacBook-Pro-2.local>
2020-04-26 13:35:54 +1000

    mysqlConnection settings
    .

--- RestaurantBooking/settings.py
+++ RestaurantBooking/settings.py
@@ -74,9 +74,14 @@ WSGI_APPLICATION = 'RestaurantBooking.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': 
+	{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SES1B',
+		'USER': 'root',
+		'PASSWORD': '',
+		'HOST': '', #blank means 'localhost'
+		'PORT': '3306',
     }
 }
 
