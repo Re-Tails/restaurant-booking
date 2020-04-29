14b40025f14b8f404dfe679a45476ee3d2dd7310
Dinh Luu <12748556@student.uts.edu.au>
2020-04-29 22:59:08 +1000

    Merged from master to current branch and added loop

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from restaurant.models import Item
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the index page.")
@@ -7,4 +8,8 @@ def index(request):
 # Create your views here.
 
 def view_menu(request):
-    return render(request, 'view_menu.html')
+    data = Item.objects.all()
+    context = {
+        'items': data
+    }
+    return render(request, 'view_menu.html', context)
