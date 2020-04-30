26599b64016881e76c88c3b09b97865b63af88da
Dinh Luu <12748556@student.uts.edu.au>
2020-04-30 18:50:29 +1000

    Updated how the dishes are displayed

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from restaurant.models import Item
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the index page.")
     return render(request, 'index.html')
 # Create your views here.
 
+
 def view_menu(request):
-    return render(request, 'view_menu.html')
+    data = Item.objects.all()
+    context = {
+        'items': data
+    }
+    return render(request, 'view_menu.html', context)
 
 def select_dish(request):
-    return render(request, 'select_dish.html')
+    data = Item.objects.all()
+    context = {
+        'items': data
+    }
+    return render(request, 'select_dish.html', context)
