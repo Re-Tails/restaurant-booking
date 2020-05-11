97b150e5e8ba41ad75b369cbefa2fdc3e3979fd3
tarzan2307 <51083472+tarzan2307@users.noreply.github.com>
2020-05-11 16:16:07 +1000

    Basic UI

--- RestaurantBooking/settings.py
+++ RestaurantBooking/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/main.css/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'direct/static'),
 ]
--- restaurant/forms.py
+++ restaurant/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=75, required=True)
 
