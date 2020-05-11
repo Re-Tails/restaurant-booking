7f4d585cd3d10a094a598961f9ba7cca7151a76e
tarzan2307 <51083472+tarzan2307@users.noreply.github.com>
2020-05-11 17:28:57 +1000

    added own login-reg branch

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-
+from django.urls import path
+from django.conf.urls import url
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -8,4 +12,11 @@ urlpatterns = [
     path('addCategory', views.addCategoryView.as_view(), name='addCategory'),
     path('addBranch', views.addBranchView.as_view(), name='addBranch'),
     path('addTable', views.addTableView.as_view(), name='addTable'),
+    path('dashboard/', views.dashboardView, name='dashboard'),
+    path('login/', LoginView.as_view(), name='login_url'),
+    path('logout/', LogoutView.as_view(), name='logout_url'),
+    path('register/', views.registerView, name='register_url'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
